9fc76fb7e4f73c37e2f473b57e8b3fd9427130cb
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-03-29 16:57:23 -0400

    updated callbacks.js with higher order functions and a callback
    I added a higher order function for each exercise. I then invoked each function with  a callback.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,19 +27,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[3]);
 }
+
+last (items, function(last) {
+  console.log(last);
+}); 
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(3, 2));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+function multiply(x, y) {
+  return x * y;
+}
+
+console.log(multiply(5, 4));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
