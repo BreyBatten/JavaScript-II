ed79f773386ae6ffa0e2ff444bd8184a4284f804
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-03-29 18:39:36 -0400

    updated array-methods.js challenge and solved, and closure.js I made my own closure
    In the array-methods.js file, I worked through challenge 3 using .filter to pull all runners that have a shirt size of L. In the closure.js file, I made my own closure to run the radio commands I use at work.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function zipCommand1() {
+  const receiver = "Set, Ready, Clear.";
+  console.log(`Radio command 1- ${receiver}`);
 
+  function zc2() {
+    const sender = "Zipping.";
+    console.log(`Radio command 2- ${sender}`);
+
+    function zc3 () {
+      const receiver = "Zip on!";
+      console.log(`Radio command 3- ${receiver}`)
+    }
+    zc3();
+  }
+  zc2();
+}
+zipCommand1();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
