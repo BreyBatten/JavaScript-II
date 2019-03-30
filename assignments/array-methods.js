9241668e594d81589203e6a29ac1fa1ae319cdec
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-03-29 23:34:08 -0400

    completed callbacks.js file
    I solved the last challenge of the callbacks file to complete my mvp of this project.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -82,6 +82,7 @@ for(let i = 0; i < runners.length; i++) {
 }
 console.log(largeShirts);
 
+
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -34,6 +34,8 @@ getLength(items, function(length) {
   console.log(length);
 });
 
+
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr[3]);
@@ -42,6 +44,8 @@ function last(arr, cb) {
 last (items, function(last) {
   console.log(last);
 }); 
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
@@ -54,6 +58,8 @@ function add(x, y) {
 
 console.log(add(3, 2));
 
+
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x, y);
@@ -65,12 +71,25 @@ function multiply(x, y) {
 
 console.log(multiply(5, 4));
 
+
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
 
-/* STRETCH PROBLEM */
+  if(list.includes(item)) {
+    cb(true);
+  }
+  else {
+    cb(false);
+  }
+}
+contains('Gum', items, function(param1){
+  console.log(param1);
+});
+
+
+/*STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
